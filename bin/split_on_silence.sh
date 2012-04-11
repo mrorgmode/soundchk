bf05575e0aba5901eb4fc592594065b515095bf8
perweij <per.weijnitz@gmail.com>
2012-04-11 14:59:13 +0200

    / ‘bin/split_on_silence.sh’

--- bin/split_on_silence.sh
+++ bin/split_on_silence.sh
@@ -5,15 +5,6 @@
 # Usage: 
 #   split_on_silence.sh inputfile outputprefix
 #
-#   After a recording sessions, call script again with same prefix, and with the
-#   -n flag, to post process the recorded files (normalisation, flac-conversion).
-#
-# Notes:
-#   Will only work on distros using pulseaudio.
-#
-# I got this from: http://www.outflux.net/blog/archives/2009/04/19/recording-from-pulseaudio/
-# and modified it slightly.
-#
 # Per Weijnitz <per.weijnitz@gmail.com>
 
 IN=$1
