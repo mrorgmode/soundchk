fd79da34af4ec8ac9aee86b0d598abeeb9eae1a9
perweij <per.weijnitz@gmail.com>
2012-04-12 10:51:20 +0200

    / ‘bin/mksong’ / ‘bin/trim_beginning_to_beat.sh’ / ‘xml/SequencerTrack.xml’ / ‘xml/Song.xml’ - ‘bin/#mksong#’ - ‘bin/.#mksong’

--- bin/trim_beginning_to_beat.sh
+++ bin/trim_beginning_to_beat.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-IN=$1
-OUT=$2
+IN="$1"
+OUT="$2"
 
 LIMIT="-20d"
 
@@ -11,16 +11,19 @@ LIMIT="-20d"
 # MAXAMP=$(sox $IN -n stat 2>&1 | grep "Maximum amplitude:" | sed 's/^.*:[[:space:]]*//')
 
 
-cp $IN $OUT
-ORGSIZE=$(stat $OUT | grep Size | sed 's/^.*Size: \([0-9]*\).*$/\1/')
-sox $OUT $IN.out.wav silence 1 0 -20d
-NEWSIZE=$(stat $IN.out.wav | grep Size | sed 's/^.*Size: \([0-9]*\).*$/\1/')
+cp "$IN" "$OUT"
+
+ORGSIZE=$(stat "$OUT" | grep Size | sed 's/^.*Size: \([0-9]*\).*$/\1/')
+sox "$OUT" "$IN.out.wav" silence 1 0 -20d
+NEWSIZE=$(stat "$IN.out.wav" | grep Size | sed 's/^.*Size: \([0-9]*\).*$/\1/')
 
 while [ $NEWSIZE -lt $ORGSIZE ]
 do
-    echo "$NEWSIZE -lt $ORGSIZE" >&2
-    sox $OUT $IN.out.wav silence 1 0 $LIMIT
+#    echo "$NEWSIZE -lt $ORGSIZE" >&2
+    sox "$OUT" "$IN.out.wav" silence 1 0 $LIMIT
     ORGSIZE=$NEWSIZE
-    NEWSIZE=$(stat $IN.out.wav | grep Size | sed 's/^.*Size: \([0-9]*\).*$/\1/')
-    mv $IN.out.wav $OUT
+    NEWSIZE=$(stat "$IN.out.wav" | grep Size | sed 's/^.*Size: \([0-9]*\).*$/\1/')
+    mv "$IN.out.wav" "$OUT"
 done
+
+exit
